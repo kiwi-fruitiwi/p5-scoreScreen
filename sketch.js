92320868d99ff36b6bda8726052c6cf70c7b0683
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-10 05:31:54 -0700

    add additional planning for actual typerc score screen

--- sketch.js
+++ sketch.js
@@ -1,21 +1,62 @@
 /**
- * @author kiwi
- * @date 2022.02.17
+ *  @author kiwi 🥝
+ *  @date 2022.04.10
  *
- * placeholder project for the score screen of TyperC, so I can get rid of
- * the planning mp4 and screenshot from my desktop :p
+ *
  */
 let font
+let instructions
+
 
 function preload() {
-    font = loadFont('data/meiryo.ttf')
+    font = loadFont('data/consola.ttf')
 }
+
 
 function setup() {
-    createCanvas(640, 360)
+    let cnv = createCanvas(600, 300)
+    cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
+    textFont(font, 14)
+
+    /* initialize instruction div */
+    instructions = select('#ins')
+    instructions.html(`<pre>
+        [1,2,3,4,5] → no function
+        z → freeze sketch</pre>`)
 }
 
-function draw() {    
+
+function draw() {
     background(234, 34, 24)
+
+
+
+    displayDebugCorner()
+}
+
+
+/** 🧹 shows debugging info using text() 🧹 */
+function displayDebugCorner() {
+    const LEFT_MARGIN = 10
+    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+    const LINE_SPACING = 2
+    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+    fill(0, 0, 100, 100) /* white */
+    strokeWeight(0)
+
+    text(`frameCount: ${frameCount}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
+    text(`frameRate: ${frameRate().toFixed(1)}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET)
+}
+
+
+function keyPressed() {
+    /* stop sketch */
+    if (key === 'z') {
+        noLoop()
+        instructions.html(`<pre>
+            sketch stopped</pre>`)
+    }
 }
